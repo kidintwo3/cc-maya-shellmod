bb790bd4b23d257dd736089b7574f0d6814f7905
kidintwo3 <kidintwo3@gmail.com>
2017-01-16 21:34:33 +0100

    added option to diable shelf creation

--- src/pluginMain.cpp
+++ src/pluginMain.cpp
@@ -17,16 +17,22 @@
 MStatus initializePlugin( MObject obj )
 {
 	MStatus status;
-    
-    icons_data_write();
+
+	
 
 	//MGlobal::executeCommand( mel_deleteShelf() );
 
 
-	MGlobal::executeCommand( mel_createShelf() );
-	MGlobal::executeCommand( mel_AETemplate() );
+	if( !std::getenv("SHELLMOD_REBUILDSHELF") )
+	{
+		icons_data_write();
+		MGlobal::executeCommand( mel_createShelf() );
+		MGlobal::executeCommand( mel_AETemplate() );
+	}
 
-	MFnPlugin fnPlugin( obj, "Creative Case", "2.04", "Any" );
+
+
+	MFnPlugin fnPlugin( obj, "Creative Case", "2.05", "Any" );
 
 	status = fnPlugin.registerCommand( "shellModCommand", ShellModCommand::creator, ShellModCommand::newSyntax );
 	CHECK_MSTATUS_AND_RETURN_IT( status );
@@ -35,13 +41,13 @@ MStatus initializePlugin( MObject obj )
 	status = fnPlugin.registerNode("shellModNode", shellModNode::id, shellModNode::creator, shellModNode::initialize);
 	CHECK_MSTATUS_AND_RETURN_IT(status);
 
-    shellModNode::pluginLoadPath = fnPlugin.loadPath(&status);
+	shellModNode::pluginLoadPath = fnPlugin.loadPath(&status);
 	CHECK_MSTATUS_AND_RETURN_IT(status);
 
-    //fnPlugin.addMenuItem("Create ShellMod", "MayaWindow|mainMeshToolsMenu", "shellModCommand", "");
-    //fnPlugin.addMenuItem("Add to ShellMod", "MayaWindow|mainMeshToolsMenu", "shellModCommand -a", "");
-    //fnPlugin.addMenuItem("Plane with ShellMod", "MayaWindow|mainMeshToolsMenu", "shellModCommand -a", "");
-    
+	//fnPlugin.addMenuItem("Create ShellMod", "MayaWindow|mainMeshToolsMenu", "shellModCommand", "");
+	//fnPlugin.addMenuItem("Add to ShellMod", "MayaWindow|mainMeshToolsMenu", "shellModCommand -a", "");
+	//fnPlugin.addMenuItem("Plane with ShellMod", "MayaWindow|mainMeshToolsMenu", "shellModCommand -a", "");
+
 	return MS::kSuccess;
 }
 
@@ -66,13 +72,13 @@ MStatus uninitializePlugin( MObject obj )
 	status = fnPlugin.deregisterNode(shellModNode::id);
 	CHECK_MSTATUS_AND_RETURN_IT(status);
 
-    
-    //MStringArray myMenu;
-    //myMenu.append( "Create ShellMod" );
-    //myMenu.append( "Add to ShellMod" );
-    //myMenu.append( "Plane with ShellMod" );
-    //fnPlugin.removeMenuItem(myMenu);
-    
+
+	//MStringArray myMenu;
+	//myMenu.append( "Create ShellMod" );
+	//myMenu.append( "Add to ShellMod" );
+	//myMenu.append( "Plane with ShellMod" );
+	//fnPlugin.removeMenuItem(myMenu);
+
 	return MS::kSuccess;
 }
 
