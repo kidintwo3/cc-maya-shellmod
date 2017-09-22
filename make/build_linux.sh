733066088c3783c1c7194773a02f515809e48e7b
kidintwo3 <kidintwo3@users.noreply.github.com>
2017-09-22 22:01:09 +0200

    make files

---
+++ make/build_linux.sh
@@ -0,0 +1,40 @@
+versions=('2016', '2016.5', '2017', '2018')
+op_system='linux'
+plugin_name='shellMod'
+make_dir='/home/none/Desktop/ShellMod/make/build/'$op_system'/'
+destination_dir='/home/none/Desktop/ShellMod/build/'$op_system'/'
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo ""
+printf "${RED}[Starting $plugin_name build...]${NC}\n"
+echo ""
+
+for i in "${versions[@]}"
+	do
+		printf '[Searching]: '$make_dir$i'\n'
+
+		if [ -e $make_dir$i ]
+		then
+			printf "${GREEN}[Found Maya v$i]${NC}\n"
+			cmake --build $make_dir$i --config Release
+
+			bundle_file=$make_dir$i'/src/'$plugin_name'.so'
+
+			if [ -e $bundle_file ]
+			then
+				mkdir -p $destination_dir$i
+				
+				cp $bundle_file $destination_dir$i
+			fi
+			
+		fi
+		
+	done
+
+
+echo ""
+printf "${GREEN}[Build finished...]${NC}\n"
+echo ""
---
+++ make/build_osx.sh
@@ -0,0 +1,30 @@
+versions=('2016' '2016.5' '2017' '2018')
+op_system='mac'
+plugin_name='shellMod'
+make_dir='/Users/hunyadijanos/Documents/GIT/cc-maya-shellmod/make/build/'$op_system'/'
+destination_dir='/Users/hunyadijanos/Documents/GIT/cc-maya-shellmod/build/'$op_system'/'
+
+
+for i in "${versions[@]}"
+	do
+		
+		if [ -e $make_dir$i ]
+		then
+			# echo $destination_dir$i
+			cmake --build $make_dir$i --config Release
+			
+			bundle_file=$make_dir$i'/src/Release/'$plugin_name'.bundle'
+			
+			
+			if [ -e $bundle_file ]
+			then
+				mkdir -p $destination_dir$i
+				
+				cp $bundle_file $destination_dir$i
+
+			fi
+			
+		fi
+		
+	done
+
